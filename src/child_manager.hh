656003399b3497c7ff25983e33d1098101483228
Antoine Damhet <antoine.damhet@lse.epita.fr>
2018-06-10 17:20:49 +0200

    use GPLv3
    Signed-off-by: Antoine Damhet <antoine.damhet@lse.epita.fr>

--- src/child_manager.hh
+++ src/child_manager.hh
@@ -1,3 +1,22 @@
+/*
+ * This file is part of tlockd.
+ *
+ * Copyright (C) 2018 Antoine Damhet
+ *
+ * tlockd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tlockd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tlockd.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef CHILD_MANAGER_HH
 #define CHILD_MANAGER_HH
 
